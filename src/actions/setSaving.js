6e8c5297803fb39eb7a948dcfc2ae5a7bdbc8dc2
Hoa Hoang <hoa@samatti.com>
2025-05-07 18:58:45 -0400

    create lib

--- src/actions/setSaving.js
+++
@@ -1,9 +0,0 @@
-export const SET_SAVING = 'SET_SAVING';
-
-const setSaving = (state, payload) => ({
-  ...state,
-  isSaving: payload.isSaving,
-  isLoadingGlobally: payload.isSaving,
-});
-
-export default setSaving;
