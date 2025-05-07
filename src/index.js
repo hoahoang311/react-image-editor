f146926c1f546ec531187bcb3392c6ec5829572a
Hoa Hoang <hoa@samatti.com>
2025-05-07 18:48:15 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import FilerobotImageEditor from './components/AssemblyPoint';
+import { TABS_IDS, TOOLS_IDS } from './utils/constants';
+
+export { TABS_IDS as TABS, TOOLS_IDS as TOOLS };
+
+export default FilerobotImageEditor;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React, {StrictMode} from "react"
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+     <div>test</div>
+ )
+ 
